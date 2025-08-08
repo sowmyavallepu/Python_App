d3135b1554181446ce4820954f2f2cbbaeeeed1d
Sowmya Vallepu <137864488+sowmyavallepu@users.noreply.github.com>
2025-08-08 16:19:33 -0500

    Update api.py

--- app/api.py
+++ app/api.py
@@ -1,15 +1,7 @@
-from fastapi import FastAPI
-from app.api import router as api_router  # Import the router
+from fastapi import APIRouter
 
-app = FastAPI()
+router = APIRouter()
 
-# Include the API router under the /api prefix
-app.include_router(api_router, prefix="/api")
-
-@app.get("/")
-async def read_root():
+@router.get("/")
+def home():
     return {"message": "Hello World from FastAPI on Azure!"}
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
