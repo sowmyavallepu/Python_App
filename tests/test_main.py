858e4a52c4b9402137b9be8ae889d92700ca6e3b
Sowmya Vallepu <137864488+sowmyavallepu@users.noreply.github.com>
2025-08-08 15:16:14 -0500

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,4 @@
-from fastapi.testclient import TestClient
-from app.main import app
-
-client = TestClient(app)
-
 def test_home():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello from FastAPI!"}
+    assert response.json() == {"message": "Hello World from FastAPI on Azure!"}
