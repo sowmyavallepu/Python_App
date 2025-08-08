bd1f1a4a8c6f22dc640b959ff969a2439e528272
Sowmya Vallepu <137864488+sowmyavallepu@users.noreply.github.com>
2025-08-08 15:32:50 -0500

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,3 +7,8 @@ def test_home():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World from FastAPI on Azure!"}
+
+def test_read_item():
+    response = client.get("/items/42")
+    assert response.status_code == 200
+    assert response.json() == {"item_id": 42}
