cd8d8aedcb32dd4f79aee0f0de7e2a684fa9848d
Sowmya Vallepu <137864488+sowmyavallepu@users.noreply.github.com>
2025-08-11 20:47:32 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,8 +8,8 @@ app.include_router(api_router, prefix="/api")
 
 @app.get("/")
 async def read_root():
-    return {"message": "Hello World from FastAPI on Azure!"}
+    return {"Message": "Hello World from FastAPI on Azure!"}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+@app.get("/items/{Item_id}")
+async def read_item(Item_id: int):
+    return {"Item_id": Item_id}
